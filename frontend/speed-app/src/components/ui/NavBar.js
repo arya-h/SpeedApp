bb1641e88fb7c024ac05411ac2cf89fa0811ac32
oscarfont <o.font@alumnos.upm.es>
2021-11-16 21:01:55 +0100

    logo added to navbar

--- frontend/speed-app/src/components/ui/NavBar.js
+++ frontend/speed-app/src/components/ui/NavBar.js
@@ -1,3 +1,4 @@
+import logo from '../../assets/speed-app-logo.png'
 
 const NavBar = () => {
 
@@ -5,7 +6,9 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">SpeedApp</a>
+                <a className="navbar-brand" href="#">
+                    <img src={logo} width="48" height="48"/>
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
