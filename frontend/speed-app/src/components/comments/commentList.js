7851cc86794736e9e3be6b8eaacc8509e9fddbd9
Álvaro Rubio Segovia <alvaro.rubio.segovia@gmail.com>
2021-11-23 15:59:32 +0100

    Messed up the structure :S

--- frontend/speed-app/src/components/comments/commentList.js
+++ frontend/speed-app/src/components/comments/commentList.js
@@ -5,18 +5,12 @@ import '../../style/comments.css';
 import { useSelector } from 'react-redux';
 
 export const CommentList = ( { idea } ) => {
-    
-    const comments = idea.comments
-    const { ideas } = useSelector((state) => state.ideas);
-
-    // Rerender when list changes
-    useEffect(() => {}, [ ideas ])
 
     return (
         <div className="comment-list-container">
-            { comments.length === 0  ?
+            { idea.comments.length === 0  ?
                 <h6>There are no comments yet</h6> : 
-                comments.map(comment => <CommentCard key={comment.id} comment={ comment }/>)
+                idea.comments.map(comment => <CommentCard key={comment.id} comment={ comment }/>)
             }
         </div>
     )
