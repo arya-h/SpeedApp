03e094b12cde2010c2d62ac9f20b860e23b302de
Álvaro Rubio Segovia <alvaro.rubio.segovia@gmail.com>
2021-11-24 22:21:43 +0100

    Minor changes

--- frontend/speed-app/src/components/ideas/IdeasScreen.js
+++ frontend/speed-app/src/components/ideas/IdeasScreen.js
@@ -10,7 +10,7 @@ export const IdeasScreen = () => {
         {/* TODO: Sidebar */}
         <div className="row">
           <div className="col-6">
-            <h1 className="my-3">Ideas</h1>
+            <h1 className="my-3">Ideas feed</h1>
           </div>
           <div className="col-6 text-end">
             <Link to="/add">
--- frontend/speed-app/src/components/ui/NavBar.js
+++ frontend/speed-app/src/components/ui/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="">Ideas Feed</a>
+                            <a className="nav-link active" aria-current="page" href="">Speed App</a>
                         </li>
                         {/* <li className="nav-item">
                             <a className="nav-link" href="#">Link</a>
