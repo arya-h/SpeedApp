c95d236c15a06c1baf470b968ed1e9dd46a5be0c
Andy Mitchell <andymitchell1@live.co.uk>
2021-11-25 11:43:44 +0100

    removed console.log

--- frontend/speed-app/src/reducers/filterReducer.js
+++ frontend/speed-app/src/reducers/filterReducer.js
@@ -3,7 +3,6 @@ import { types } from "../types/types";
 export const filterReducer = (state = "", action) => {
   switch (action.type) {
     case types.updateFilter: {
-        console.log(state)
       return action.payload;
     }
 
