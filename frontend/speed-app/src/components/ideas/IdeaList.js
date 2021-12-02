1ad45f6a75f40ceefc8f3975947a1f4582ba2a81
Andy Mitchell <42553271+amitchell94@users.noreply.github.com>
2021-12-02 17:27:49 +0100

    moved filter dropdown to ideasScreen (#29)

--- frontend/speed-app/src/components/ideas/IdeaList.js
+++ frontend/speed-app/src/components/ideas/IdeaList.js
@@ -3,10 +3,9 @@ import { Col, Row, Dropdown, Form } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { IdeaCard } from "./IdeaCard";
 
-export const IdeaList = () => {
+export const IdeaList = ({ sortMethod, setSortMethod }) => {
   const filter = useSelector((state) => state.filter);
   const { ideas } = useSelector((state) => state.ideas);
-  const [sortMethod, setSortMethod] = useState(0);
 
   const filteredIdeas = ideas.filter((x) =>
     x.title.toLowerCase().includes(filter.toLowerCase())
@@ -24,7 +23,7 @@ export const IdeaList = () => {
       if (a.title.toLowerCase() > b.title.toLowerCase()) {
         return 1;
       }
-      return 0
+      return 0;
     });
   } else if (sortMethod === 3) {
     filteredIdeas.sort((a, b) => {
@@ -34,38 +33,21 @@ export const IdeaList = () => {
       if (a.title.toLowerCase() > b.title.toLowerCase()) {
         return -1;
       }
-      return 0
+      return 0;
     });
   }
   // Rerender when list changes (Delete idea)
   useEffect(() => {}, [ideas]);
 
   return (
-    <div style={{height:"85%", overflowY:"scroll"}}>
+    <div style={{ height: "85%", overflowY: "scroll" }}>
       {ideas.length === 0 ? (
         <h1>There are no ideas yet</h1>
       ) : (
         <>
-          <Row className="mb-2">
-            <Col>
-              <Form.Select
-                className="w-auto ms-auto"
-                onChange={(event) =>
-                  setSortMethod(parseInt(event.target.value))
-                }
-              >
-                <option value="0">Date: Newest first</option>
-                <option value="1">Date: Oldest first</option>
-                <option value="2">Title: A-Z</option>
-                <option value="3">Title: Z-A</option>
-              </Form.Select>
-            </Col>
-          </Row>
-          <Row>
-            {filteredIdeas.map((i) => (
-              <IdeaCard key={i.id} idea={i} />
-            ))}
-          </Row>
+          {filteredIdeas.map((i) => (
+            <IdeaCard key={i.id} idea={i} />
+          ))}
         </>
       )}
     </div>
