076be458cc1cffab8a03ec00f159ed6396436f7f
Andy Mitchell <andymitchell1@live.co.uk>
2021-12-02 13:39:08 +0100

    added sort by title name or date feature, added AM/PM to date format

--- frontend/speed-app/src/components/ui/UserDateDisplay.js
+++ frontend/speed-app/src/components/ui/UserDateDisplay.js
@@ -9,7 +9,7 @@ export const UserDateDisplay = ( { user, date }) => {
             <i className={"fas fa-user-circle"} style={{fontSize: "2.25rem", paddingTop: "1rem"}}/>
             <div className={"post__body"}>
                 <span className={"post__username"}>{ user?.name }</span> 
-                <span className={"post__date"}>{ moment( date ).format('MMMM Do YYYY, h:mm:ss') }</span>
+                <span className={"post__date"}>{ moment( date ).format('MMMM Do YYYY, h:mm:ss A') }</span>
             </div>
         </div>
     )
