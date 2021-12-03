9810e6e9e67ed2bf50d45621e99faffaae008090
oscarfont <o.font@alumnos.upm.es>
2021-12-03 01:14:40 +0100

    user email added

--- frontend/speed-app/src/components/ui/UserProfileInfo.js
+++ frontend/speed-app/src/components/ui/UserProfileInfo.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { startLogout } from '../../actions/auth';
 import '../../style/components/userprofile.css';
+import { getAuth } from "firebase/auth";
 
 const UserProfileInfo = ( { user }) => {
 
     const dispatch = useDispatch();
+    const userEmail = getAuth().currentUser?.email;
     
     const handleLogout = () => {
         dispatch( startLogout() );
@@ -16,7 +18,7 @@ const UserProfileInfo = ( { user }) => {
             <i className={"fas fa-user-circle"} style={{fontSize: "5rem", paddingTop: "2rem"}}/>
             <div className={"info-container"}>
                 <span className={"username"}>{ user?.name }</span> 
-                <span className={"email"}>pedro@fakeemail.com</span>
+                <span className={"email"}>{userEmail}</span>
             </div>
             <button 
                 className="btn btn-danger ms-2 mb-3"
